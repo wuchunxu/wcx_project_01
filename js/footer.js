3886fe89f64770a1550f78032cdcace42fc4d784
wuchunxu <wcxcapricorn@foxmail.com>
2018-04-14 18:37:47 +0800

    1

--- js/footer.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by web-01 on 2018/4/14.
- */
-$(function(){
-    //创建link标签，导入footer.css文件
-    $(`<link rel="stylesheet" href="css/footer.css"></link>`).appendTo(document.head);
-
-    $("#footer").load("footer.html");
-});
--- js/header.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by web-01 on 2018/4/14.
- */
-$(function(){
-    $(`<link rel="stylesheet" href="css/header.css">`).appendTo(document.head);
-    $("#nav").load("header.html");
-});
